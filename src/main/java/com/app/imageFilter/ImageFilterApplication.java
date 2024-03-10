26e0f6def87c5f84dd2006dabb239bf9f9e9ed99
Ubuntu <ubuntu@ip-172-31-89-189.ec2.internal>
2024-03-10 07:07:59 +0000

    some files added

---
+++ src/main/java/com/app/imageFilter/ImageFilterApplication.java
@@ -0,0 +1,13 @@
+package com.app.imageFilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/imageFilter/ImageFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.imageFilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
